fab0fbfa2e6291443d9d8d913599c88fc4dafc49
ma1orek <idzikbartosz@gmail.com>
2025-06-20 11:58:29 +0200

    TRIUMF: Ostateczna implementacja z poprawną biblioteką i modelem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function GifSwapTool() {
     const [gifFile, setGifFile] = useState<File | null>(null);
